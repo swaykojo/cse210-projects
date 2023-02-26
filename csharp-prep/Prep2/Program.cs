c47f3cee6297751a2de37b1b3b4325baf6364940
swaykojo <awuahgodsway@gmail.com>
2023-02-26 17:01:21 +0000

    gradesUpdates

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -6,47 +6,63 @@ namespace Prep2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is Prep 2");
+         Console.WriteLine("This is prep 2");
+         
+         Console.Write("What is your grade percentage? ");
+         string userInput = Console.ReadLine();
+         int gradeNumber = int.Parse(userInput); 
+         
+         string letter = ""; 
 
-            Console.Write("What is your grade percentage? ");
-            string grade= Console.ReadLine();
+         int remainder = gradeNumber % 10; 
 
-            int x = int.Parse(grade);
-            
-            string letter = "";
-           
-            if (x >= 90)
-               {
-                letter = "A";
-               }
-            else if (x >=80)
-              {
-                letter = "B";
-              }
-            else if (x >=70)
-               {
-                 letter = "C";
-               }
-            else if (x >= 60)
-               {
-                 letter = "D";
-               }
-            else 
-               {
-                letter = "f";
-               }
-               Console.WriteLine($"Your letter grade is: {letter}");
-                 
-                 if (x >=70)
-                     {
-                    Console.WriteLine("Congratulations! You passed the class!");
-                    }
-                 else 
-                    {
-                       Console.WriteLine("Stay focused and you'll get it next time!");
-                    }
-        } 
+         string signGrade = "";
+
+         if (gradeNumber >= 90) {
+            letter = "A";
+         } 
+         else if (gradeNumber >= 80) {
+            letter = "B";
+         } 
+         else if (gradeNumber >= 70) {
+            letter = "C";
+         } 
+         else if (gradeNumber >= 60) {
+            letter = "D";
+         } 
+         else {
+            letter = "F";  
+         } 
         
-          
+        if (remainder >= 7){
+             signGrade = "+";
+        } 
+        else if (remainder < 3) {
+          signGrade = "-";
+        } 
+       
+       //Recognize that there is no A+ grade, only A and A-.
+       //Add some additional logic to your program to detect this case and handle it correctly.
+        if (remainder >=7 && gradeNumber >= 97) {
+         signGrade = "";
+        } 
+        else if (remainder < 3 && gradeNumber < 93) {
+         signGrade = "-";
+        } 
+        //Similarly, recognize that there is no F+ or F- grades, only F. 
+        //Add additional logic to your program to detect these cases and handle them correctly.
+        if (gradeNumber < 60) {
+         signGrade = "";
+        }
+        Console.WriteLine($"Your grade is {letter}{signGrade}");
+         // Accessing if the user has pass the exam 
+         if(gradeNumber >= 70) {
+            Console.Write("Congratulation you passed the exam.");
+         } 
+         else {
+            Console.Write("Everyone fails at some point. You will do better the next time.");
+         }
+
+        }
     }
 }
